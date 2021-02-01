6034174414f31ff855a321dbb3abd864f913ad80
unknown <costa.sergio.86@gmail.com>
2021-02-01 19:44:48 +0000

    Error Handling in the Frontend

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -4,40 +4,63 @@ const Create = () => {
 
     const [description, setDescription] = useState('')
     const [file, setFile] = useState(null)
+    const [error, setError] = useState('')
 
     console.log(file)
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
+        if(!description) {
+            setError('Please add a descripiton')
+            return
+        }
+
+        if(!file) {
+            setError('Please add a file')
+            return
+        }
+
         const formData = new FormData()
         formData.append('data', JSON.stringify({description}))
         formData.append('files.image', file)
         
-        const response = await fetch('http://localhost:1337/posts', {
-            method: 'POST',
-            body: formData
-        })
+        try {
+            const response = await fetch('http://localhost:1337/posts', {
+                method: 'POST',
+                body: formData
+            }) 
 
-        const data = await response.json()
+            const data = await response.json()
 
-        console.log(data)
+            console.log("data", data)
+            
+        } catch (err) {
+            console.log("Exception", err)
+            setError(err)
+        }
     }
     
     return (
         <div className="Create">
             <h2>Create</h2>
-
+            {error && <p>{error}</p>}
             <form onSubmit={handleSubmit}>
                 <input 
                     placeholder="Description" 
                     value={description}
-                    onChange={(e) => setDescription(e.target.value)}
+                    onChange={(e) => {
+                        setError('')
+                        setDescription(e.target.value)
+                    }}
                 />
                 <input  
                     type="file"
                     placeholder="Add a image"
-                    onChange={(e) => setFile(e.target.files[0])}
+                    onChange={(e) => {
+                        setError('')
+                        setFile(e.target.files[0])
+                    }}
                 />
                 <button>Submit</button>
             </form>
