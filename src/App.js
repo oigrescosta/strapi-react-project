87d177786c98b6dfc373dde9799c899e695c3c23
unknown <costa.sergio.86@gmail.com>
2021-02-04 22:06:37 +0000

    Custom Nav for logged/logged out in users + Login Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Create from './pages/Create'
 import SinglePost from './pages/SinglePost'
+import Login from './pages/Login'
 import Nav from "./components/Nav"
 
 import './App.css';
@@ -18,6 +19,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/create" exact component={Create} />
+          <Route path="/login" exact component={Login} />
           <Route path="/:id" exact component={SinglePost} />
         </Switch>
       </BrowserRouter>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {NavLink} from 'react-router-dom'
+import {UserContext} from '../context/UserContext'
 
 const Nav = () => {
+
+    const {user} = useContext(UserContext)
+
     return (
         <div className="Nav">
             <NavLink to="/" exact> Home </NavLink>
-            <NavLink to="/create" exact> Create </NavLink>
+            {user && <NavLink to="/create" exact> Create </NavLink>}
+            {!user && <NavLink to="/login" exact> Login </NavLink>}
         </div>
     )
 }
-
+ 
 export default Nav
