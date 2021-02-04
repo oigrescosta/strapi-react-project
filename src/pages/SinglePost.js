e9865764063d32439c6edb0d5064cc004ad79ca0
unknown <costa.sergio.86@gmail.com>
2021-02-04 21:32:22 +0000

    Delete and Update Post Request

--- src/pages/SinglePost.js
+++ src/pages/SinglePost.js
@@ -1,21 +1,53 @@
 import React, {useState, useEffect} from 'react'
 import Post from '../components/Post'
 
-const SinglePost = ({match}) => {
+const SinglePost = ({match, history}) => {
     const {id} = match.params
     console.log("id", id)
 
     const [post, setPost] = useState({})
     const [loading, setLoading] = useState(true)
+    const [edit, setEdit] = useState(false)
+
+    const [description, setDescription] = useState('')
+
+    const fetchPost = async () => {
+        const response = await fetch(`http://localhost:1337/posts/${id}`)
+        const data = await response.json()
+        console.log("data", data)
+        setPost(data)
+        setDescription(data.description)
+        setLoading(false)
+    }
+
+    const handleDelete = async () => {
+        const response = await fetch(`http://localhost:1337/posts/${id}`, {
+            method: 'DELETE'
+        })
+        const data = await response.json()
+        history.push('/')
+    }
+
+    const handleEditSubmit = async (event) => {
+        event.preventDefault()
+        console.log("handleEditSubmit")
+
+        const response = await fetch(`http://localhost:1337/posts/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                description
+            })
+        })
+
+        const data = await response.json()
+        fetchPost()
+        console.log("handleEditSubmit data", data)
+    }
 
     useEffect(() => {
-        const fetchPost = async () => {
-            const response = await fetch(`http://localhost:1337/posts/${id}`)
-            const data = await response.json()
-            console.log("data", data)
-            setPost(data)
-            setLoading(false)
-        }
         fetchPost()
     }, [])
 
@@ -26,11 +58,25 @@ const SinglePost = ({match}) => {
             {!loading &&   
                 <>    
                     {post.id && 
-                        <Post 
-                            description={post.description}
-                            url={post.image && post.image.url}
-                            likes={post.likes} 
-                        />
+                        <>
+                            <Post 
+                                description={post.description}
+                                url={post.image && post.image.url}
+                                likes={post.likes} 
+                            />
+                            <button onClick={handleDelete}>Delete this Post</button> 
+                            <button onClick={() => setEdit(true)}>Edit this Post</button> 
+                            {edit &&
+                                <form onSubmit={handleEditSubmit}>
+                                    <input 
+                                        value={description} 
+                                        onChange={(event) => setDescription(event.target.value)} 
+                                        placeholder="New description" 
+                                    />
+                                    <button>Confirm</button>
+                                </form>
+                            }
+                        </>
                     }  
 
                     {!post.id && <p>404 - Page not found</p>}      
